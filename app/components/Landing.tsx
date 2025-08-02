72393c7de78c8f804bca7f9c19eecd4082abc68a
KB3D6K4E3Q <ondrikzubek12@gmail.com>
2025-08-02 18:04:49 +0200

    Redeploy all files

--- app/components/Landing.tsx
+++ app/components/Landing.tsx
@@ -50,9 +50,9 @@ export default function Home() {
   return (
 
     <motion.main
-      variants={slideUp}
-      initial="initial"
-      animate="enter"
+        initial={{ opacity: 0, y: 100 }}
+  animate={{ opacity: 1, y: 0 }}
+  transition={{ duration: 0.8 }}
       className="relative flex h-screen overflow-hidden"
     >
       <Image
